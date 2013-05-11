68afed782a667d70c3b25a1d42c1f6d81e1b930a
Kaspars Rinkevics <rinkevics@gmail.com>
2013-05-11 13:43:02 +0300

    trash

--- Scripts.js
+++ Scripts.js
@@ -4,7 +4,6 @@
 		$(".trash").droppable({
 			tolerance: "pointer", 
 			drop: function( event, ui ) {
-				alert(1);
 				$(ui.draggable).remove();
 			},
 		});
@@ -25,6 +24,14 @@
      
       $( "#draggable6" ).draggable({ cursor: "move", snap: true, containment: "parent", 
         grid: [ 39,39 ] });
+		
+		
+		$("#add_kitchen").click(function() {
+			$elem = $( "#kitchen" ).clone();
+			$elem.removeClass("hideme");
+			setDrag($elem);
+			$elem.appendTo("#sandbox");
+		});
 		
 
     });
@@ -50,3 +57,11 @@
         document.getElementById('SqSum').innerHTML = "Total m<sup>2</sup>: " + SqSumma.toFixed(2);
         document.getElementById('TotalPrice').innerHTML = "Total price:" + (SqSumma * 1000).toFixed(2) + "Eur";
     }
+	
+	
+	function setDrag(d) {
+			d.draggable({ 
+				cursor: "move", snap: true, containment: "parent", 
+				grid: [ 39,39 ] }).resizable({ aspectRatio: true,  grid: 39, resize: function( event, ui ){resizedBlock(event, ui)} });
+			
+		}
